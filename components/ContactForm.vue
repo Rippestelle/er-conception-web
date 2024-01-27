f9885e93d2da647a4ab4eb02ecabdfa701db225e
Estelle Rippe <rippestelle@gmail.com>
2024-01-27 17:49:55 +0100

    remove script

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -48,24 +48,7 @@
   </form>
 </template>
 
-<script lang="ts">
-const handleSubmit = (event) => {
-  event.preventDefault();
-
-  const myForm = event.target;
-  const formData = new FormData(myForm);
-
-  fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString(),
-  })
-    .then(() => console.log("Form successfully submitted"))
-    .catch((error) => alert(error));
-};
-
-document.querySelector("form").addEventListener("submit", handleSubmit);
-</script>
+<script lang="ts"></script>
 
 <style lang="scss" scoped>
 form {
